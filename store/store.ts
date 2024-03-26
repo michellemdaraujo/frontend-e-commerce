23889fa8b5d9807cad22265bb1dd98c15d1cd63e
michellemdaraujo <mmd-araujo@hotmail.com>
2024-03-26 18:25:43 -0300

    chore: Remove debug from reducer

--- store/store.ts
+++ store/store.ts
@@ -61,7 +61,7 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   Action<string>
 >;
 
-export const wrapper = createWrapper(makeStore, { debug: true });
+export const wrapper = createWrapper(makeStore);
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
